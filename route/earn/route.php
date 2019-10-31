5f01e73f093d40e38d1cbbbff46af7c4a41e38c9
Administrator <354148728@qq.com>
2019-10-31 12:17:19 +0800

    add run-crawl-task route

--- route/earn/route.php
+++ route/earn/route.php
@@ -7,5 +7,6 @@ Route::any('hotel/search', 'earn/Hotel/search');
 Route::resource('hotel', 'earn/Hotel')->middleware('authentication');
 
 # task
+Route::any('run-crawl-task', 'earn/Task/runCrawlTask')->middleware('authentication');
 Route::resource('task', 'earn/Task')->middleware('authentication');
 
