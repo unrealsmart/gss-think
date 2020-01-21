a35c61d52879b5e3ae1a31ff681470ea7b6272e0
Administrator <354148728@qq.com>
2020-01-21 23:29:04 +0800

    main route change

--- route/main/route.php
+++ route/main/route.php
@@ -2,10 +2,21 @@
 
 use tauthz\facade\Enforcer;
 use think\facade\Route;
-use Utils\Casbin;
+
+
+Route::any('/t/1', function () {
+    return 't/1';
+})->middleware('authentication');
+Route::any('/t1/1', function () {
+    return 't1/1';
+})->middleware('authentication');
 
 // test
 Route::any('test', function () {
+
+//    dump(lang(null));
+//    return 1;
+
     // Casbin::createRole(['admin']);
     //
     // dump(Enforcer::addPolicy('role:test', 'domain:test', '*', '(.*)', 'TEST'));
@@ -19,17 +30,20 @@ Route::any('test', function () {
     // u 更新
     // a 所有
 
-    // /t read
-    // dump(Enforcer::addPolicy('role:admin', 'domain:main', '/t*', 'read'));
-    // dump(Enforcer::addGroupingPolicy('user:alice', 'role:admin', 'domain:main'));
-    // dump(Enforcer::getRolesForUserInDomain('user:alice', 'domain:main'));
-    // dump(Enforcer::enforce('user:alice', 'domain:main', '/t/1', 'read'));
+//    dump(Enforcer::addPolicy('role:admin', 'domain:main', '/t*', 'r'));
+//    dump(Enforcer::addGroupingPolicy('user:alice', 'role:admin', 'domain:main'));
+//    dump(Enforcer::getRolesForUserInDomain('user:alice', 'domain:main'));
+//    dump(Enforcer::enforce('user:alice', 'domain:main', '/t/1', 'r'));
+//
+//    dump(Enforcer::addPolicy('role:admin2', 'domain:main2', '*', 'a'));
+//    dump(Enforcer::addGroupingPolicy('user:alice2', 'role:admin2', 'domain:main2'));
+//    dump(Enforcer::getRolesForUserInDomain('user:alice2', 'domain:main2'));
+//    dump(Enforcer::enforce('user:alice2', 'domain:main2', '/1', 'r'));
 
-    // /a all
-    // dump(Enforcer::addPolicy('role:admin', 'domain:main', '/a/*', 'all'));
-    // dump(Enforcer::addGroupingPolicy('user:alice', 'role:admin', 'domain:main'));
-    // dump(Enforcer::getRolesForUserInDomain('user:alice', 'domain:main'));
-    dump(Enforcer::enforce('user:alice', 'domain:main', '/a/1', 'write'));
+//    dump(Enforcer::addPolicy('role:admin', 'domain:main', '/a/*', 'all'));
+//    dump(Enforcer::addGroupingPolicy('user:alice', 'role:admin', 'domain:main'));
+//    dump(Enforcer::getRolesForUserInDomain('user:admin', 'domain:main'));
+//    dump(Enforcer::enforce('user:admin', 'domain:main', '/main/t/*', 'a'));
 
     return;
 });
@@ -60,7 +74,8 @@ Route::resource('domain', 'main/Domain');
 // role
 Route::resource('role', 'main/Role');
 
-
+// authority
+Route::resource('authority', 'main/Authority');
 
 
 
@@ -70,14 +85,3 @@ Route::any('administrator/verification', 'main/Administrator/verification');
 Route::resource('administrator', 'main/Administrator');
     //->middleware('authentication');
 
-
-
-// authority
-Route::resource('authority', 'main/Authority')->middleware('authentication');
-
-
-// authentication
-Route::any('authentication/test', 'demo/demo')->middleware('authentication');
-
-
-// authority
